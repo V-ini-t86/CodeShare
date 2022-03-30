bac674a65c7ff23ae19d77ffb6e3086313917b19
Nikhil4597 <bramanrohit@gmail.com>
2022-03-30 19:29:55 +0530

    Message

--- ShareEditor/routes/index.js
+++ ShareEditor/routes/index.js
@@ -5,14 +5,34 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'CodeEditor' });
 });
+
 router.get('/about', function(req, res, next) {
   res.render('about', { title: 'About This website' });
 });
-router.route('/contact')
-.get(function(req, res, next) {
-  res.render('contact', { title: 'CodeShare Platform is a good for dry run the code.'});
+
+router.route('/contact').get(function(req,res,next){
+  res.render('contact',{title : 'Contact us'});
 })
-.post(function (req,res,next) {
-  res.render('thank',{ title : 'CodeShare Platform is a good for dry run the code.'});
-}) 
+.post(function(req,res,next){
+  req.checkBody('name','Invalid name').notEmpty();
+  req.checkBody('email','Invalid email').isEmail();
+  req.checkBody('number','Invalid number').notEmpty();
+  req.checkBody('message','Empty message').notEmpty();
+  var errors = req.getValidationErrors();
+  if(errors)
+  {
+    res.render('error',{
+      title:'Error',
+      name:req.body.name,
+      email:req.body.email,
+      number:req.body.number,
+      message:req.body.message,
+      errorMessage:errors
+    
+  })
+  }
+  else
+  res.render('out',{title :'Thank you'});
+});
+
 module.exports = router;
